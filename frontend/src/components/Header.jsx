c4df6765975c72d0df8c07713da08eee461848e8
Sameer Manku <167529700+sameermanku@users.noreply.github.com>
2025-04-26 16:04:25 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,10 +10,8 @@ function Header() {
                 <div className="header-top">
                     <div className="container-fluid">
                         <div className="row">
-                            {/* <div className="col-md-6"> */}
-                            <img src="/img/icons/clock.png" alt="Clock" />
+                            <img src={`${process.env.PUBLIC_URL}/img/icons/clock.png`} alt="Clock" />
                             <p>Mon - Sat 09:00am - 08:00pm</p>
-                            {/* </div> */}
                         </div>
                     </div>
                 </div>
@@ -21,13 +19,13 @@ function Header() {
                 <div className="site-navbar">
                     {/* Logo */}
                     <a href="/" className="site-logo">
-                        <img src="/img/logo.png" alt="Logo" />
+                        <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="Logo" />
                     </a>
 
                     <div className="header-right">
                         <div className="header-info-box">
                             <div className="hib-icon">
-                                <img src="/img/icons/phone.png" alt="Phone" />
+                                <img src={`${process.env.PUBLIC_URL}/img/icons/phone.png`} alt="Phone" />
                             </div>
                             <div className="hib-text">
                                 <h6>+91 70091-89097</h6>
@@ -36,11 +34,11 @@ function Header() {
                         </div>
                         <div className="header-info-box">
                             <div className="hib-icon">
-                                <img src="/img/icons/map-marker.png" alt="Location" />
+                                <img src={`${process.env.PUBLIC_URL}/img/icons/map-marker.png`} alt="Location" />
                             </div>
                             <div className="hib-text">
-                            <h6>Shop No. 118, Chora Bazar</h6>
-                            <p>Chajju Majari, Old Rajpura (Punjab)</p>
+                                <h6>Shop No. 118, Chora Bazar</h6>
+                                <p>Chajju Majari, Old Rajpura (Punjab)</p>
                             </div>
                         </div>
                     </div>
@@ -49,19 +47,19 @@ function Header() {
                     <nav className="site-nav-menu">
                         <ul>
                             <li className={location.pathname === "/" ? "active" : ""}>
-                            <Link to="/">Home</Link>
+                                <Link to="/">Home</Link>
                             </li>
                             <li className={location.pathname === "/about" ? "active" : ""}>
-                            <Link to="/about">About us</Link>
+                                <Link to="/about">About us</Link>
                             </li>
                             <li className={location.pathname === "/Services" ? "active" : ""}>
-                            <Link to="/Services">Services</Link>
+                                <Link to="/Services">Services</Link>
                             </li>
-                            {/*<li className={location.pathname === "/portfolio" ? "active" : ""}>*/}
-                            {/*<Link to="/portfolio">Portfolio</Link>*/}
-                            {/*</li>*/}
+                            {/* <li className={location.pathname === "/portfolio" ? "active" : ""}>
+                                <Link to="/portfolio">Portfolio</Link>
+                            </li> */}
                             <li className={location.pathname === "/contact" ? "active" : ""}>
-                            <Link to="/contact">Contact</Link>
+                                <Link to="/contact">Contact</Link>
                             </li>
                         </ul>
                     </nav>
